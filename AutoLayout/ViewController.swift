d2c27a7676628fdb8cd31fc3297b8654f4b633f4
minhtien1403 <minhtiencp1403@gmail.com>
2021-02-19 09:49:33 +0700

    Hoan thanh autolayout cho man hinh key qua va cac phim chuc nang

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Tiến on 2/19/21.
+//  Copyright © 2021 Tiến. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
